fab0de2cd86592b1ead122a791d05f0495f6bfbe
Robert Blixt <info@robertblixt.com>
2020-02-13 06:45:14 +0100

    Making elapsed time less visible

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,30 +1,9 @@
-function show_result -d "Shows the result of the previous command, or the duration if successful"
-  set -l status_copy $status
-
-  # if test "$CMD_DURATION" -gt 20
+function show_result -d "Shows duration of previous command"
     set -l duration (echo $CMD_DURATION | humanize_duration)
-
-    if test ! -z "$duration"
-        set -l indicator
-
-        #if test $status_copy -ne 0
-        #    set indicator " $status_copy "
-        #end
-
-        segment_right white 282B2C " $duration "
-        # segment_close
-    end
-  #else if test $status_copy -ne 0
-  #  segment_right white f00 " $status_copy "
-  #  # segment_close
-  #else
-  #  segment_right eee 282B2C (date +%H:%M)" "
-  #  # segment_close
-  #end
+    segment_right 777777 282B2C " $duration "
 end
 
 function fish_right_prompt -d "Prints right prompt"
   show_result
-
   segment_close
 end
