43d93224e4d90a2fc833257b8e1ddacf5eb8600e
Robert Blixt <robert@zivriodesktop.lan>
2021-05-25 18:44:45 +0200

    Remove elapsed time

--- functions/fish_right_prompt.fish
+++
@@ -1,9 +0,0 @@
-function show_result -d "Shows duration of previous command"
-    set -l duration (echo $CMD_DURATION | humanize_duration)
-    segment_right 777777 282B2C " $duration "
-end
-
-function fish_right_prompt -d "Prints right prompt"
-  show_result
-  segment_close
-end
